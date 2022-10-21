f068bcbe715bdde86213d287dd1f8a18f4b6359b
hyoung311 <72768097+hyoung311@users.noreply.github.com>
2022-10-21 15:33:51 -0600

    add new changes 4

--- background.js
+++ background.js
@@ -1,6 +1,11 @@
 console.log('background running');
 
-chrome.action.onClicked.addListener(buttonClicked);
+chrome.action.onClicked.addListener((tab) => {
+    chrome.scripting.executeScript({
+      target: {tabId: tab.id},
+      files: ['content.js']
+    });
+  });
 
 function buttonClicked(tab) {
     console.log(tab);
