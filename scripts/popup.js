cda0b33425401ca44f41cc170200fc1862d8f410
hyoung311 <72768097+hyoung311@users.noreply.github.com>
2022-10-24 13:13:41 -0600

    fixed rotation i guesssss

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,22 +1,28 @@
-console.log("This is a popup!!");
+console.log("This is a popup!!! poop");
 
-window.onload = choosePic();
+
 
 var randomNum = 0;
 var myPix = new Array("images/image1.jpeg","images/image2.jpeg","images/image3.jpeg","images/image4.jpeg","images/image5.jpeg","images/image6.jpg","images/image7.jpeg");
 
-function choosePic() {
-    var randomNum = Math.floor(Math.random() * myPix.length);
-    document.getElementById("myPicture").src = myPix[randomNum];
-    rotate();
-   }
+console.log("hi!" + myPix.length);
+console.log(myPix);
 
 function rotate() {
     randomNum++;
     if (randomNum == myPix.length) {
         randomNum = 0;
     }
-//    document.getElementById("myPicture").src = myPix[randomNum];
+    document.getElementById("myPicture").src = myPix[randomNum];
 
     setTimeout(rotate, 3 * 1000);
+//    rotate();
 }
+
+window.onload = rotate();
+
+function choosePic() {
+    var randomNum = Math.floor(Math.random() * myPix.length);
+    document.getElementById("myPicture").src = myPix[randomNum];
+    rotate();
+   }
