2b330871563425fe397bec4d586037c62d21d7d8
hyoung311 <72768097+hyoung311@users.noreply.github.com>
2022-10-24 12:24:19 -0600

    cleaned up folders

---
+++ scripts/background.js
@@ -0,0 +1,15 @@
+console.log('background running!');
+
+
+//not in use yet, still messing around
+
+// chrome.action.onClicked.addListener((tab) => {
+//     chrome.scripting.executeScript({
+//       target: {tabId: tab.id},
+//       files: ['popup.js']
+//     });
+//   });
+
+// function buttonClicked(tab) {
+//     console.log(tab);
+// }
---
+++ scripts/popup.js
@@ -0,0 +1,22 @@
+console.log("This is a popup!!");
+
+window.onload = choosePic();
+
+var randomNum = 0;
+var myPix = new Array("images/image1.jpeg","images/image2.jpeg","images/image3.jpeg","images/image4.jpeg","images/image5.jpeg","images/image6.jpg","images/image7.jpeg");
+
+function choosePic() {
+    var randomNum = Math.floor(Math.random() * myPix.length);
+    document.getElementById("myPicture").src = myPix[randomNum];
+    rotate();
+   }
+
+function rotate() {
+    randomNum++;
+    if (randomNum == myPix.length) {
+        randomNum = 0;
+    }
+//    document.getElementById("myPicture").src = myPix[randomNum];
+
+    setTimeout(rotate, 3 * 1000);
+}
